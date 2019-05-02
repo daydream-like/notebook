a3e9e6454392174b4f6fb1cb978f981c7e3ebae7
likechris <likechris@didichuxing.com>
2019-05-02 18:18:50 +0800

    feat_test:add queue func and test case

--- leetcode_like/queue.js
+++ leetcode_like/queue.js
@@ -1,3 +1,41 @@
+/**
+ * 队列（先进先出）
+ * 在队尾插入元素，在队首删除元素，用于存储按顺序排列的元素
+ * 
+ */
 class Queue {
-
+    constructor() {
+        this.dataStore = [];
+    }
+    enqueue(element) {
+        //向队尾添加元素
+        this.dataStore.push(element)
+    }
+    dequeue() {
+        //删除队首元素
+        this.dataStore.shift();
+    }
+    front() {
+        // 队首
+        this.dataStore[0];
+    }
+    back() {
+        this.dataStore[this.dataStore.length - 1]
+    }
+    empty() {
+        if (this.dataStore.length === 0) {
+            return true;
+        }
+        return false;
+    }
+    toString() {
+        let retStr = "";
+        for (var i = 0; i < this.dataStore.length; ++i) {
+            retStr += this.dataStore[i] + "\n";
+        }
+        return retStr;
+    }
 }
+
+
+module.exports = Queue;
---
+++ leetcode_like/queue.test.js
@@ -0,0 +1,16 @@
+let Queue = require('./queue');
+let expect = require('chai').expect;
+const assert = require('assert');
+
+
+describe('队列测试', function () {
+    it('出队入队测试', function () {
+        let q = new Queue();
+        q.enqueue('like');
+        expect(q.dataStore).to.deep.equal(['like'])
+        q.enqueue('like2')
+        expect(q.dataStore).to.deep.equal(['like', 'like2'])
+        q.dequeue()
+        expect(q.dataStore).to.deep.equal(['like2'])
+    })
+})
