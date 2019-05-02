0af133ac92cdea177bb3b4497b63fa9f7fbfd8c6
likechris <likechris@didichuxing.com>
2019-05-03 02:09:36 +0800

    refactor:delete some unuse code

--- leetcode_like/sort.js
+++ leetcode_like/sort.js
@@ -7,19 +7,15 @@ function bubbleSort(arr) {
     for (let i = 0; i < len; i++) {
         for (let j = 0; j < len - i; j++) {
             if (arr[j] > arr[j + 1]) {
-                console.log(arr[j],arr[j+1],arr)
-                let temp  = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                console.log(arr[j], arr[j + 1], arr)
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
     return arr;
 }
-
-console.log(bubbleSort([1,2,5,3,4,90,12,32,121,111]))
-
-
 module.exports = {
     bubbleSort
 }
