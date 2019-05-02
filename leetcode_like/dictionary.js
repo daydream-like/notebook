08d0618dc9ad0ad7f9a362b2fb9bcd1bf49ba868
likechris <likechris@didichuxing.com>
2019-05-02 19:45:17 +0800

    feat:add dictionary

--- leetcode_like/dictionary.js
+++ leetcode_like/dictionary.js
@@ -1,3 +1,17 @@
-class Dictionary{
+class Dictionary {
+    constructor() {
+        this.dataStore = [];
+    }
+    add(key, value) {
+        this.dataStore[key] = value;
+    }
+    find(key) {
+        return this.dataStore[key]
+    }
+    remove(key) {
+        delete this.dataStore[key]
+    }
 
 }
+
+module.exports = Dictionary;
