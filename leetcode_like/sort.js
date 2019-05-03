037641acc5275f7a8bd0552b8ac57b7c69511d60
likechris <likechris@didichuxing.com>
2019-05-03 17:11:56 +0800

    feat+test:add quickSort

--- leetcode_like/sort.js
+++ leetcode_like/sort.js
@@ -27,7 +27,7 @@ function swap(arr, index, min) {
 }
 function selectionSort(arr) {
     let min;
-    for (let i = 0; i < arr.length-1; i++) {
+    for (let i = 0; i < arr.length - 1; i++) {
         min = i;
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[min] > arr[j]) {
@@ -38,7 +38,52 @@ function selectionSort(arr) {
     }
     return arr;
 }
+
+/**
+ * shell排序
+ */
+
+function shellsort(arr) {
+    var N = arr.length;
+    var h = 1;
+    while (h < N / 3) {
+        h = 3 * h + 1;
+    }
+    while (h >= 1) {
+        for (var i = h; i < N; i++) {
+            for (var j = i; j >= h && arr[j] < arr[j - h];
+                j -= h) {
+                swap(arr, j, j - h);
+            }
+        }
+        h = (h - 1) / 3;
+    }
+    return arr;
+}
+
+/**
+ * 快速排序
+ */
+function quickSort(arr) {
+    if (arr.length === 0) {
+        return [];
+    }
+    var pivot = arr[0];
+    var left = [];
+    var right = [];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            right.push(arr[i])
+        } else {
+            left.push(arr[i])
+        }
+    }
+    return quickSort(left).concat(pivot, quickSort(right))
+}
+
 module.exports = {
     bubbleSort,
-    selectionSort
+    selectionSort,
+    shellsort,
+    quickSort
 }
--- leetcode_like/sort.test.js
+++ leetcode_like/sort.test.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 const assert = require('assert');
-const { bubbleSort, selectionSort } = require('./sort.js');
+const { bubbleSort, selectionSort, quickSort } = require('./sort.js');
 
 describe('排序测试', function () {
     it('冒泡排序测试', function () {
@@ -8,7 +8,10 @@ describe('排序测试', function () {
         expect(bubbleSort([5, 4, 3, 1, 2])).to.deep.equal([1, 2, 3, 4, 5])
         expect(bubbleSort([1, 4, 3, 5, 2])).to.deep.equal([1, 2, 3, 4, 5])
     })
-    it('快速排序测试', function () {
+    it('选择排序测试', function () {
         expect(selectionSort([1, 5, 2, 3, 4, 8, 7])).to.deep.equal([1, 2, 3, 4, 5, 7, 8])
+    })
+    it('快速排序测试', function () {
+        expect(quickSort([1, 5, 2, 3, 4])).to.deep.equal([5, 4, 3, 2, 1])
     })
 })
