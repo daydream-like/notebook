26d11b398dd37c3bc9e97fede0fbd598a30e3706
likechris <likechris@didichuxing.com>
2019-09-16 14:52:20 +0800

    feat(leetcode): twoSum

--- leetcode_like/twoSum.test.js
+++ leetcode_like/twoSum.test.js
@@ -5,5 +5,7 @@ const { twoSum } = require('./twoSum');
 describe('twoSum', function () {
     it('twoSum', function () {
         expect(twoSum([1, 2, 3, 4, 5], 3)).to.deep.equal([1, 0]);
+        expect(twoSum([1, 2, 3, 4, 5], 4)).to.deep.equal([2, 0]);
+        expect(twoSum([1, 2, 3, 4, 5], 10)).to.deep.equal(undefined);
     })
 })
